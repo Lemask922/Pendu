aa19cfdc22f76931415b5c434a6cf78242a025ef
Ayoub139 <aboudaayoub99@gmail.com>
2023-11-24 10:57:59 +0100

    Reload new code

--- Pendu.c
+++ Pendu.c
@@ -1,11 +1,14 @@
 //
-// Created by Oussaama Ammar El Idrissi on 17/11/2023.
+// Created by aboudaay on 17/11/2023.
 //
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
 #include"Pendu.h"
 #include<string.h>
+#include <time.h>
+#include <ctype.h>
+
+
 char tab[100][15] = {
         "Aventure", "Énigme", "Mystère", "Fantaisie", "Magie",
         "Labyrinthe", "Exploration", "Evasion", "Conquête", "Héros",
@@ -28,51 +31,44 @@ char tab[100][15] = {
         "Divin", "Délire", "Astre", "Mirage", "Illimité",
         "Extase", "Divin", "Délire", "Astre", "Mirage"
 };
-
-void afficherCadre(int largeur) {
-    for (int i = 0; i < largeur; i++) {
-        printf("-");
-    }
-    printf("\n");
-}
-
-void afficherPendu() {
-    // Affiche le cadre supérieur
-    afficherCadre(26);
-
-    // Affiche les lignes du milieu avec le dessin du pendu
-    printf("|                          |\n");
-    printf("|                          |\n");
-    printf("|");
-    // Dessin du pendu
-    printf(" +------+      \n");
-    printf(" |     |\n");
-    printf(" |     O\n");
-    printf(" |    /|\\\n");
-    printf(" |    / \\\n");
-    printf("_|_\n");
-    printf(" Le pendu!\n");
-    printf("|                          |\n");
-    printf("|                          |\n");
-
-    // Affiche le cadre inférieur
-    afficherCadre(26);
-}
-
 char* Word(char tab[100][15])
 {
     srand(time(NULL)); // initialisation de la fonction rand
     int value = rand() % 101; // intervalle de la fonction
     char* result = malloc(15 * sizeof(char)); // allouer de la mémoire pour le resultat
 
-    strncpy(result, tab[value], 15); //copie de chaine de caractère dans une autre avec la limite de 15
+    strncpy(result, tab[value], 14); // Copy up to 14 characters from tab[value] to result
+    result[14] = '\0'; // Null-terminate the result string
     return result;
 }
-void Mot_Cache(char* res)
+void Mot_Cache(char* res, char* tmp)
 {
     int L = strlen(res);
     for (int i = 0; i < L; i++)
     {
-        printf("-");
+        tmp[i]='-';
+    }
+    tmp[L] = '\0';
+}
+
+void deviner(char* res, char* tmp, bool* verif) {
+    char Lettre;
+    *verif = false;
+
+    printf("Saisir une lettre : \n");
+    scanf(" %c", &Lettre);
+
+    for (int i = 0; i < strlen(res); i++) {
+        if (toupper(Lettre) == toupper(res[i])) {
+            tmp[i] = res[i];
+            *verif = true;
+        }
+    }
+
+    if (!(*verif)) {
+        printf("La lettre %c n'est pas dans le mot.\n", Lettre);
     }
 }
+//
+// Created by Abouda on 23/11/2023.
+//
--- main.c
+++ main.c
@@ -3,20 +3,35 @@
 #include"Pendu.h"
 #include<string.h>
 
-
 int main() {
 
-    printf("Bienvenue dans le jeu du pendu!\n");
-
-    // Appel de la fonction pour afficher le pendu
-    afficherPendu();
-
     char* res;
+    char tmp[MAX_LEN + 1];
     res = Word(tab);
+    int life = 6;
+    bool verif;
 
-    printf("Le nom tire par hasard = %s \n", res);
+    printf("Le nom tire par hasard est  = %s\n", res);
 
-    Mot_Cache(res);
+    Mot_Cache(res, tmp);
+    printf("%s\n", tmp);
+
+    while (life > 0 && strcmp(res, tmp) != 0) {
+        deviner(res, tmp, &verif);
+        printf("%s\n", tmp);
+
+        if (!verif) {
+            life--;
+            printf("Il vous reste %d vies.\n", life);
+        }
+    }
+
+    if (strcmp(res, tmp) == 0) {
+        printf("Félicitations ! Vous avez deviné le mot : %s\n", res);
+    } else {
+        printf("Désolé, vous avez épuisé toutes vos vies. Le mot était : %s\n", res);
+    }
+
     free(res);
     return 0;
 }
